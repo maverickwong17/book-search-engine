50688de7b42e1c8e310fb18dc90604a18dfcd504
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-08-29 18:25:01 -0700

    update auth; update all schemas; apollo client install

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,32 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 function App() {
   return (
+    <ApolloProvider client = {client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +40,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,32 +4,32 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        getSingleUser: async (parent, { user = null,params }) => {
-            return User.findOne({
-                $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
-              });
-        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id }).populate('thoughts');
+              }
+              throw new AuthenticationError('You need to be logged in!');
+            },
     },
 
     Mutation:{
-        createUser: async (parent, { body }) => {
-            user = await User.create(body);
+        createUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
         },
-        login: async (parent, { body }) => {
-            const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
             if (!user) {
-                throw new AuthenticationError('No user found with this email address');
+              throw new AuthenticationError('No user found with this email address');
             }
             const correctPw = await user.isCorrectPassword(password);
             if (!correctPw) {
-                throw new AuthenticationError('Incorrect credentials');
+              throw new AuthenticationError('Incorrect credentials');
             }
             const token = signToken(user);
-      
             return { token, user };
-        },
+          },
         saveBook: async (parent, { user, body }, context) => {
             console.log(user);
             if (context.user){
@@ -41,7 +41,7 @@ const resolvers = {
             }
             throw new AuthenticationError('You need to be logged in!');
         },
-        deleteBook: async (parent, {user, params}, context) => {
+        removeBook: async (parent, {user, params}, context) => {
             if (context.user) {
                 return  User.findOneAndUpdate(
                 { _id: user._id },
